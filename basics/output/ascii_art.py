79b987aa1cba29ac5287941f68b8e1f0fbf042ad
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-05 11:37:48 +0000

    Added code to display an ascii art robot.

---
+++ basics/output/ascii_art.py
@@ -0,0 +1,29 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+
+
+
+
+
+# Beep the Robot
+print("\t\t\t\t\t\t\t\t\t\t\t\t###")
+print("\t\t\t\t\t\t\t\t\t\t\t##########")
+print("\t\t\t\t\t\t\t\t\t\t\t# o   o  #")
+print("\t\t\t\t\t\t\t\t\t\t\t#   #    #")
+print("\t\t\t\t\t\t\t\t\t\t\t# \\___/  #")
+print("\t\t\t\t\t\t\t\t\t\t\t##########")
+print("\t\t\t\t\t\t\t\t\t\t\t   +++")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t ################")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t   #  #")
+print("\t\t\t\t\t\t\t\t\t\t\t  #    #")
+print("\t\t\t\t\t\t\t\t\t\t\t #      #")
