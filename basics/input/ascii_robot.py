bb9c94850dfea9d76113664e6a9b37b43d967074
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-08 09:55:42 +0000

    Added code to display an ASCII robot with dynamic eyes.

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,22 @@
+
+# Beep thgit e Robot with user input
+print("Please enter a character for the eye:")
+eye = input()
+
+print("\t\t\t\t\t\t\t\t\t\t\t\t###")
+print("\t\t\t\t\t\t\t\t\t\t\t##########")
+print(f"\t\t\t\t\t\t\t\t\t\t\t# {eye}   {eye}  #")
+print("\t\t\t\t\t\t\t\t\t\t\t#   #    #")
+print("\t\t\t\t\t\t\t\t\t\t\t# \\___/  #")
+print("\t\t\t\t\t\t\t\t\t\t\t##########")
+print("\t\t\t\t\t\t\t\t\t\t\t   +++")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t ################")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t    #")
+print("\t\t\t\t\t\t\t\t\t\t\t   #  #")
+print("\t\t\t\t\t\t\t\t\t\t\t  #    #")
+print("\t\t\t\t\t\t\t\t\t\t\t #      #")
