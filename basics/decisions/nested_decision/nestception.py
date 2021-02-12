5a48018e998f089bd958afacd5d1708283d1bd79
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-12 14:42:05 +0000

    Updated comments to code that was added  to demonstrate the use of a nested decision statement.

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -1,14 +1,20 @@
 # Helping Beep find its spare battery using multiple nested decisions
 
+# Retrieve which room user wants to look in
 print ("Where should I look?")
 response = input()
 
 # A multiple nested if statement to determine which room the user has suggested to look in.
 # Once a room has been identified another question is asked to determine an area to look within the room,
 # appropriate responses will be given depending on users input at each statement.
+
+# Identify room
 if response == "in the bedroom":
     print ("Where in the bedroom should I look?")
     bedroom_area = input()
+
+    # Identify area to look in
+
     if bedroom_area == "under the bed":
         print ("Found some shoes but no battery")
     else:
@@ -17,6 +23,9 @@ if response == "in the bedroom":
 elif response == "in the bathroom":
     print ("Where in the bathroom should I look?.")
     bathroom_area = input()
+
+    # Identify area to look in
+
     if bathroom_area == "in the bathtub":
         print ("Found a rubber duck but no battery.")
     else:
@@ -25,6 +34,9 @@ elif response == "in the bathroom":
 elif response == "in the lab":
     print ("Where in the lab should I look?")
     lab_area = input()
+
+    # Identify area to look in
+
     if lab_area == "on the table":
         print ("Yes! I found my battery!")
     else:
