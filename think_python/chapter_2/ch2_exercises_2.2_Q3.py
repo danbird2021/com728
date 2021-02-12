43ab440ad9deab0a09d62fa9f671f01379c859cb
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-12 08:09:15 +0000

    Added update to chapter 2 Exercises from Think Python

--- think_python/chapter_2/ch2_exercises_2.2_Q3.py
+++ think_python/chapter_2/ch2_exercises_2.2_Q3.py
@@ -2,43 +2,47 @@
 # then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again,
 # what time do I get home for breakfast?
 
-start_time = 6.52
+start_time = 06.5200
 print(f"Start time: {start_time}am")
 
-easy_pace = 08.15
+
+easy_pace = .0815
 print(f"Easy pace: {easy_pace} per mile")
+
 
 easy_pace_distance = 2
 print(f"Total miles done on easy pace: {easy_pace_distance} miles")
 
-easy_pace_elapsed_decimal = easy_pace * easy_pace_distance
-print(f"Elapsed time on easy pace (decimal): {easy_pace_elapsed_decimal}")
 
-easy_pace_elapsed_mins = easy_pace_elapsed_decimal
+easy_pace_elapsed = easy_pace * easy_pace_distance
+print(f"Elapsed time on easy pace: {easy_pace_elapsed}")
 
-
-print(f"Elapsed time on easy pace (minutes): {easy_pace_elapsed_mins}")
-
-fast_pace = 07.12
+fast_pace = .0725
 print(f"Up tempo pace: {fast_pace} per mile")
+
 
 fast_pace_distance = 3
 print(f"Total miles done on up tempo pace: {fast_pace_distance} miles")
 
-fast_pace_elapsed_decimal = fast_pace * fast_pace_distance
-print(f"Elapsed time on fast pace: {fast_pace_elapsed_decimal}")
+
+fast_pace_elapsed = fast_pace * fast_pace_distance
+print(f"Elapsed time on fast pace: {fast_pace_elapsed}")
 
 
-fast_pace_elapsed_mins = fast_pace_elapsed_decimal
+time_elapsed =
+#= easy_pace_elapsed + fast_pace_elapsed
+print(f"Time elapsed: {time_elapsed}")
 
-print(f"Elapsed time on fast pace (minutes): {fast_pace_elapsed_mins}")
+# end_time = start_time - time_elapsed
+# print(f"End time:{end_time}")
 
-time_elapsed = easy_pace_elapsed_mins + fast_pace_elapsed_mins
 
-print(f"Time elapsed (decimal): {time_elapsed}")
 
-#hours = int(time_elapsed)
+hours = int(time_elapsed)
 minutes = (time_elapsed*60) % 60
 seconds = (time_elapsed*3600) % 60
+print(hours)
+print(minutes)
+print(seconds)
 
-print("Time elapsed (minutes):%02d.%02d" % (minutes, seconds))
+print("Time elapsed:%02d:%02d.%02d" % (hours,minutes, seconds))
