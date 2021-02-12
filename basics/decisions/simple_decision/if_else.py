3e68dbd7fdd7de86d3d47f9b52bf7810112a4e84
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-12 10:16:09 +0000

    Added code to demonstrate the use of an if...else statement.

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,13 @@
+#a program to allow user to suggest activities to beep for it to do
+
+print("Please enter the activity to be performed:")
+activity = input()
+
+# an if else statement to determine if user enters "calculate" and to give correct response
+if activity == "calculate":
+    print("Performing Calculations...")
+else:
+    print("Performing activity...")
+
+print("Done!")
+
