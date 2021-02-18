7f8676c5ea9c962b4e03055440149cfec3d130ba
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-18 18:22:14 +0000

    Updated code to Chapter 3.1  Exercises from Think Python

--- think_python/chapter_3/ch3_exercises_3.1.py
+++ think_python/chapter_3/ch3_exercises_3.1.py
@@ -3,7 +3,40 @@
 
 
 def right_justify(s):
-    print (' ' * (70 - (len(s))),s)
+    #print (' ' * (70 - (len(s))),s)
 
-# Run Function right_justify with variable Dan
+Run Function right_justify with variable Dan
 right_justify('Dan')
+
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+def print_lyrics():
+    print("I'm a lumberjack and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+repeat_lyrics()
+
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+
+line1 = "Bing tiddle "
+line2 = "tiddle bang."
+
+cat_twice(line1,line2)
+
+
+
+
+
+
