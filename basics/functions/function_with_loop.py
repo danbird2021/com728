c1dbd74192b372fe552ab823e2018227a42d994c
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-19 13:37:07 +0000

    Added code to demonstrate the use of a user-defined function with a loop.

---
+++ basics/functions/function_with_loop.py
@@ -0,0 +1,19 @@
+# A program to demonstrate the use of a user-defined function with a loop.
+
+def cross_bridge(distance):
+    """
+    Retrieves amount of steps from user, using a for loop to display 'Crossed step.' for each iteration.
+    Finally an if statement to display certain responses depending on amount of steps received.
+    """
+    for s in range(distance):
+        print("Crossed step.")
+
+    if distance >=5:
+        print("The bridge is collapsing!")
+    else:
+        print("We must keep going!")
+
+
+
+cross_bridge(3)
+cross_bridge(6)
