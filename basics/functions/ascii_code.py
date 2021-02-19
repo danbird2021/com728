7a1835101a0c690bced129385440a8ec00868cdc
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-19 07:35:09 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,23 @@
+# A program that determines the ASCII code for a character using the built in function ord().
+
+# Display start message
+print("Program Started!")
+
+# Retrieve character to determine ASCII code.
+print("Please enter a standard character:")
+character = input()
+
+# Determine ASCII code for character retrieved and display result.
+
+# Character retrieved must be a single character otherwise display error message.
+if len(character) == 1:
+
+    value = ord(character)
+    print(f"The ASCII code for {character} is: {value}.")
+
+    # Program finished message.
+    print("Program Ended!")
+
+# Display error message.
+else:
+    print("Sorry, you must only enter a single character!")
