644d437121fcfe9408be6d1b098992a264f3ef53
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-19 11:11:42 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,16 @@
+# A program using a simple function and an if statement which is called
+# to return a response to the user depending on the input.
+
+# Define Listen function.
+def identify():
+    # Retrieve what was seen by user.
+    print("What do you see?")
+    saw = input()
+    # Determine if a large boulder was seen and return the appropriate response.
+    if saw == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+# Call listen function.
+identify()
