a44f211c0582df684dd6f995b3b73977ecfd23e0
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-19 13:50:07 +0000

    Added code to demonstrate the use of a user-defined function with multiple parameters.

---
+++ basics/functions/function_with_parameters.py
@@ -0,0 +1,16 @@
+# A program to demonstrate the use of user-defined functions with multiple parameters
+
+def climb_ladder(steps_remaining,steps_crossed):
+    """
+    Retrieve amount of steps remaining and amount of steps crossed,
+    and determine if amount of steps remaining is greater than steps crossed.
+    Finally display a correct response.
+    """
+    if steps_remaining > steps_crossed:
+        print("Still some way to go!")
+    else:
+        print("We are almost there!")
+
+
+climb_ladder(5, 2)
+climb_ladder(2, 5)
