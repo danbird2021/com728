5b045cd37645b18eb6dc852acb2c77b6e5a692d1
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-23 13:34:49 +0000

    Updated code to Hangman 2 game to remove minor syntax errors

--- think_python/fun/hangman_game2.py
+++ think_python/fun/hangman_game2.py
@@ -1,10 +1,5 @@
-# Hangman Game
-
-
-
-def lives_left(guesses_left,display_word, word, guessed_letters):
-
-
+# Hangman Game Version 2
+def lives_left(guesses_left, display_word, word, guessed_letters):
     if guesses_left <= 10:
         base = "__________"
     else:
@@ -59,23 +54,23 @@ def lives_left(guesses_left,display_word, word, guessed_letters):
        \nWord: {display_word}
        \n{guesses_left} lives left remaining.
         """)
-    if guesses_left <= 0:
+    if display_word == word or guesses_left <= 0:
         exit()
     elif guesses_left > 0:
         get_guess(guesses_left, display_word, word, guessed_letters)
 
 
-def get_guess(guesses_left,display_word,word, guessed_letters):
+def get_guess(guesses_left, display_word, word, guessed_letters):
     print("Please enter a single letter to guess:")
     guess = input().strip().lower()
-    check_guess(guess,word,guesses_left,display_word, guessed_letters)
+    check_guess(guess, word, guesses_left, display_word, guessed_letters)
 
 
-def check_guess(guess,word,guesses_left,display_word,guessed_letters):
+def check_guess(guess, word, guesses_left, display_word, guessed_letters):
     correct = False
     old_display_word = display_word
-    display_word  = ""
-    guessed_letters += (f" {guess[0]}")
+    display_word = ""
+    guessed_letters += f" {guess[0]}"
 
     for position in range(0, len(word), 1):
         if word[position] == guess[0]:
@@ -84,23 +79,25 @@ def check_guess(guess,word,guesses_left,display_word,guessed_letters):
         else:
             display_word += old_display_word[position]
 
-    if  correct == False and guesses_left == 1:
+    if correct is False and guesses_left == 1:
         print("You didn't guess correctly and you lose!")
         print("GAME OVER!")
         print(f"The correct word was: {word}")
         lives_left(0, display_word, word, guessed_letters)
-    elif correct == False and guesses_left > 1:
+
+    elif correct is False and guesses_left > 1:
         guesses_left -= 1
         print("You didn't guess correctly!")
 
         lives_left(guesses_left, display_word, word, guessed_letters)
 
-    elif correct == True:
+    elif correct is True:
         print("You guessed correctly!")
+
         if display_word == word:
             print("Congratulations - You've won!!")
             print(f"The Correct word was: {word}.")
-            exit()
+            lives_left(guesses_left, display_word, word, guessed_letters)
         elif guesses_left == 11:
             print(display_word)
             get_guess(guesses_left, display_word, word, guessed_letters)
@@ -109,22 +106,13 @@ def check_guess(guess,word,guesses_left,display_word,guessed_letters):
 
 
 def get_word():
-    print("Welcome to the Hangman Game!")
+    print("Welcome to the Hangman 2 Game!")
     print("\nPlease enter word to be used:")
     word = input().strip().lower()
     guessed_letters = ""
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     print(f"The word has {len(word)} letters.")
-    get_guess(11, '_' * len(word) , word, guessed_letters)
+    get_guess(11, '_' * len(word), word, guessed_letters)
 
 
 get_word()
-
-
-
-
-
-
-
-
-
