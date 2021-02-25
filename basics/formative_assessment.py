cdfa5bc734c7d80c5ae9a7f9784a7550b45cb029
Daniel Bird <4birdd46@solent.ac.uk>
2021-02-25 11:01:10 +0000

    Added code for Block A Formative assessment

---
+++ basics/formative_assessment.py
@@ -0,0 +1,27 @@
+def myth():
+    print("Who wishes to learn about the demigod Maui?")
+    name = input()
+    print(f"Well, {name}, he stole the heart of the goddess Te Fiti.")
+    print("You must find Maui and lift the curse!")
+
+
+
+
+def explore(site):
+    if site == "The Cavern":
+        print("You have found a camakau! You can sail the seas with this.")
+    else:
+        print("This is just another part of the village.")
+
+
+
+def voyage(distance):
+    for d in range(distance):
+        print("...Crossed some ocean!")
+    print("You have found Maui on an island!")
+
+
+myth()
+explore("The Cavern")
+explore("The Hut")
+voyage(3)
