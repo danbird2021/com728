44f892d31495c69153e9f66f295c120b272e4623
Daniel Bird <4birdd46@solent.ac.uk>
2021-03-01 13:35:49 +0000

    Updated comment to CWD.py file

--- data/files/cwd.py
+++ data/files/cwd.py
@@ -1,6 +1,17 @@
+# A program to display the information about the current working directory.
+
+# Import os module
 import os
 
+
 def cwd():
+
+"""
+Retrieve the file path of the current working directory and place it into the variable 'path',
+using the getcwd method of module os. Display path back to user.
+Using a for loop and listdir method of module os, retrieve and display to user the
+content of the current working directory.
+"""
  path = os.getcwd()
  print(f"The current working directory: {path}")
  print("The directory contains the following files:")
@@ -8,8 +19,12 @@ def cwd():
   print(file)
 
 def run():
+ """
+ Display to user processing message and call cwd function.
+ :return:
+ """
  print("Processing...")
  cwd()
 
-
+# Call run function.
 run()
