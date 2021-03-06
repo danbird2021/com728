d3d8f8035f077f1286dde45e62902a7b39487eec
Daniel Bird <4birdd46@solent.ac.uk>
2021-03-06 12:59:47 +0000

    Added code to display the number of passengers that survived.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -36,7 +36,15 @@ def display_passenger_names():
         passenger_name = record[3]
         print(f"{passenger_name}")
 
+def display_num_survivors():
+    num_survived = 0
 
+    for record in records:
+        survival_status = int(record[1])
+        if survival_status == 1:
+            num_survived += 1
+
+    print(f"{num_survived} passengers survived")
 
 def run():
     load_data("titanic.csv")
@@ -46,6 +54,8 @@ def run():
     print(f"You have selected option: {selected_option}\n")
     if selected_option == 1:
         display_passenger_names()
+    elif selected_option == 2:
+        display_num_survivors()
     else:
         print("Error! Option not recognised!")
     #print(headings)1
