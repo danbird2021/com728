727f6f98398931b9ca083534b9a5b824ad51d0e1
Daniel Bird <4birdd46@solent.ac.uk>
2021-03-06 12:50:52 +0000

    Added code to display names of passengers.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -5,7 +5,7 @@ records = []
 headings = []
 
 def load_data(file_path):
-    print("Loading data...")
+    print("Loading data...", end="")
     with open(file_path) as file:
         csv_reader = csv.reader(file)
 
@@ -18,25 +18,38 @@ def load_data(file_path):
 
 def display_menu():
     print("""
-    Please select one of the following options:
-    [1] Display the names of all passengers
-    [2] Display the number of passengers that survived
-    [3] Display the number of passengers per gender
-    [4] Display the number of passengers per age group
+Please select one of the following options:
+[1] Display the names of all passengers
+[2] Display the number of passengers that survived
+[3] Display the number of passengers per gender
+[4] Display the number of passengers per age group
   
 
     """)
     menu_choice = int(input())
     return menu_choice
 
+def display_passenger_names():
+    print(f"The names of the passengers are...\n")
+    passenger_name = ""
+    for record in records:
+        passenger_name = record[3]
+        print(f"{passenger_name}")
+
+
+
 def run():
     load_data("titanic.csv")
     print(f"Successfully loaded {len(records)} records.")
 
     selected_option = display_menu()
-    print(f"You have selected option: {selected_option}")
-
-
+    print(f"You have selected option: {selected_option}\n")
+    if selected_option == 1:
+        display_passenger_names()
+    else:
+        print("Error! Option not recognised!")
+    #print(headings)1
+    #print(records)
 # Determine if main file being executed.
 if __name__ == "__main__":
     run()
