687ebc3c15d2ea2933bc6707cc3741b8fc2fb632
Daniel Bird <4birdd46@solent.ac.uk>
2021-03-20 17:18:18 +0000

    Added code to retrieve a single record.

---
+++ persistence/select_one.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def retrieve_bot():
+    db = sqlite3.connect("bots.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM bots")
+    single_row = cursor.fetchone()
+    db.close
+    print(single_row)
+
+def run():
+    retrieve_bot()
+
+# Determine if main file is being executed.
+if __name__ == "__main__":
+    run()
