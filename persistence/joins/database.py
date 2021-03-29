c2bd11a5def6bb69fae1f00539848e652bc036b3
Daniel Bird <4birdd46@solent.ac.uk>
2021-03-29 17:20:12 +0100

    Added code to retrieve data using an inner join.

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -3,18 +3,29 @@ import sqlite3
 def display_products_with_stock_levels():
     db = sqlite3.connect("catalogue.db")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM product NATURAL JOIN stock")
+    cursor.execute("SELECT product.name, product.description, stock.quantity FROM product NATURAL JOIN stock")
     all_rows = cursor.fetchall()
     num_of_products = 0
     db.close()
 
 
-    print(f"There are {len(all_rows)} products in the catalogue.")
+    print(f"\nThere are {len(all_rows)} products in the catalogue.")
     print(f"The stock level for each product is as follows:\n")
     for row in all_rows:
-        print(f"""Product: {row[1]}
-Description: {row[2]}
-Stock level: {row[3]}\n""")
+        print(f"""Product: {row[0]}
+Description: {row[1]}
+Stock level: {row[2]}\n""")
+
+
+def display_product_supplier():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT product.name, supplier.name  FROM product INNER JOIN supplier ON product.supplier_id = supplier.id")
+    all_rows = cursor.fetchall()
+    db.close()
+
+    for row in all_rows:
+        print(f"Product: {row[0]}, Supplier: {row[1]}")
         
     
 
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -2,7 +2,9 @@ import database
 
 def menu():
     print(f"""Please select one of the following options:
-[1] Display stock levels\n""")
+    
+[1] Display stock levels
+[2] Display suppliers\n""")
 
     response = int(input("Your selection:"))
 
@@ -14,6 +16,8 @@ def run():
 
     if menu_choice == 1:
         database.display_products_with_stock_levels()
+    elif menu_choice == 2:
+        database.display_product_supplier()
 
 # Determine if main file is being executed.
 if __name__ == "__main__":
